152e02f57f9e5d191fa53a987edfedefb7d15b91
Daniel Lamando <apt@danopia.net>
2018-02-04 00:24:45 -0800

    More Panel UI, plus cookie setting/clearing

---
+++ firebase/public/panel/app.js
@@ -0,0 +1,54 @@
+
+Vue.component('auth-card', {
+  template: '#auth-card',
+  data: () => {
+    const secretKey = `skychart.${orbiter.launcher.chartName}.secret`;
+    return {
+      secretKey: secretKey,
+      launchSecret: '',
+      savedSecret: localStorage[secretKey],
+    };
+  },
+  created() {
+    promise.then(() => this.fetchSecret());
+  },
+  methods: {
+    fetchSecret() {
+      skylink.get('/persist/launch-secret')
+        .then(x => this.launchSecret = x.StringValue);
+    },
+    setCookie() {
+      localStorage[this.secretKey] = this.launchSecret;
+      this.savedSecret = this.launchSecret;
+    },
+    deleteCookie() {
+      delete localStorage[this.secretKey];
+      this.savedSecret = null;
+    },
+  },
+});
+
+Vue.component('irc-net-card', {
+  template: '#irc-net-card',
+  props: {
+    config: Object,
+  },
+  computed: {
+    autoConnect() {
+      return this.config['auto-connect'] != 'no';
+    },
+    useTls() {
+      return this.config['use-tls'] == 'yes';
+    },
+  },
+});
+
+Vue.component('irc-prefs-card', {
+  template: '#irc-prefs-card',
+  data: () => {
+    return {
+      enableNicklist: false,
+      layout: 'modern',
+    };
+  },
+});
