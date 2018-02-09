770a151ee38ed1cd5fefa2c8cca2c15af26f0566
Daniel Lamando <apt@danopia.net>
2018-02-08 23:03:21 -0800

    irc/web: Start export tool. approx 3% done

---
+++ firebase/public/irc/export.js
@@ -0,0 +1,28 @@
+Vue.component('export-tool', {
+  template: '#export-tool',
+  data() {
+    return {
+      network: '',
+      context: '',
+      clicked: false,
+    };
+  },
+  methods: {
+    preview() {
+      if (this.clicked) {
+        alert('oh and now you think i programmed preview too. cute.');
+      } else {
+        alert('lol you thought i coded preview already?');
+        this.clicked = true;
+      }
+    },
+    submit() {
+      if (this.clicked) {
+        alert('oh and now you think i programmed download too. cute.');
+      } else {
+        alert('lol you thought i coded download already?');
+        this.clicked = true;
+      }
+    },
+  },
+});
