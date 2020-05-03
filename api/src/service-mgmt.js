4df9f205e62a04bbcb836154d53158201d22f13c
Daniel Lamando <dan@danopia.net>
2020-05-03 18:25:46 +0200

    api: Damnit

--- api/src/service-mgmt.js
+++ api/src/service-mgmt.js
@@ -249,7 +249,7 @@ class UserService {
       this.serviceMgmt.frameListeners.set(newDoc.path, function (x) {
         clearTimeout(timeout);
         resolve(x);
-      );
+      });
     });
 
     return {
